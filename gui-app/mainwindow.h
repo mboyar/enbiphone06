04343c24b1812fd919d6a8611d39a5bcb766dc36
Murat Boyar <muratboyar@gmail.com>
2018-01-07 13:46:40 +0300

    upd

--- gui-app/mainwindow.h
+++ gui-app/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QSerialPort>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +16,41 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+
+    void on_pushButton_1_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_5_clicked();
+
+    void on_pushButton_6_clicked();
+
+    void on_pushButton_7_clicked();
+
+    void on_pushButton_8_clicked();
+
+    void on_pushButton_9_clicked();
+
+    void on_pushButton_asterisk_clicked();
+
+    void on_pushButton_0_clicked();
+
+    void on_pushButton_sharp_clicked();
+
+    void on_pushButton_clear_clicked();
+
+    void on_pushButton_call_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QString phoneNumber;
+    QSerialPort *modemCommands;
+
 };
 
 #endif // MAINWINDOW_H
