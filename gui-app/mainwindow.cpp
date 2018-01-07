f0047e84bf0312b04bdc20598ca60f296b987553
Murat Boyar <muratboyar@gmail.com>
2018-01-07 14:15:48 +0300

    upd

--- gui-app/mainwindow.cpp
+++ gui-app/mainwindow.cpp
@@ -101,7 +101,7 @@ void MainWindow::on_pushButton_clear_clicked()
 
 void MainWindow::on_pushButton_call_clicked()
 {
-    QByteArray cmdATD = "ATD" + phoneNumber.toUtf8() + ";";
+    QByteArray cmdATD = "ATD" + phoneNumber.toUtf8() + ";" + '\n\r';
     modemCommands->write(cmdATD);
 
     qDebug() << "phoneNumber: " << phoneNumber;
